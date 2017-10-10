87d23804a27cb938bf29e953e3a9acb28f3a97a0
MAIMAI <m7a2i8@gmail.com>
2017-10-09 17:21:40 -0700

    Test

--- app/src/main/java/ali_class/localnotification/Constants.java
+++ app/src/main/java/ali_class/localnotification/Constants.java
@@ -2,7 +2,6 @@ package ali_class.localnotification;
 
 
 public class Constants {
-
     public static String ACTION_DISMISS = "action_dismiss";
     public static String ACTION_SNOOZE = "action_snooze";
 }
